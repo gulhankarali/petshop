b9130aa9d9f5b66aecf9510d974ac033539ec8bc
GulhanKarali <gulhan.karali@gmail.com>
2020-02-04 19:08:46 +0000

    git

--- src/test/java/jdbctests/jdbc_examples.java
+++ src/test/java/jdbctests/jdbc_examples.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import java.sql.*;
 
 public class jdbc_examples {
+
     String dbUrl = "jdbc:oracle:thin:@3.94.251.137:1521:xe";
     String dbUsername="hr";
     String dbPassword="hr";
@@ -49,8 +50,38 @@ public class jdbc_examples {
         while (resultSet.next()) {
             System.out.println(resultSet.getString("first_name"));
         }
+        resultSet.beforeFirst();
+        resultSet.absolute(4);
+        System.out.println(resultSet.getObject("last_name"));
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+    @Test
+    public void metadata() throws SQLException {
+
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("select * from employees");
+
+        DatabaseMetaData dbMetaData= connection.getMetaData();
+        System.out.println("user "+ dbMetaData.getUserName());
+        System.out.println("Database product name "+dbMetaData.getDatabaseProductName());
+        System.out.println("Database product version "+ dbMetaData.getDatabaseProductVersion());
+        System.out.println("Driver "+dbMetaData.getDriverName());
+        System.out.println("Driver version "+dbMetaData.getDriverVersion());
+
+        ResultSetMetaData rsMetaData= resultSet.getMetaData();
+        System.out.println("Count coloums "+rsMetaData.getColumnCount());
+        System.out.println("Column names "+rsMetaData.getColumnName(2));
+        System.out.println("Column names "+rsMetaData.getColumnName(8));
 
 
+        for (int i=1; i<rsMetaData.getColumnCount();i++) {
+            System.out.println("Column names " + rsMetaData.getColumnName(i));
+        }
 
         resultSet.close();
         statement.close();
---
+++ src/test/java/jdbctests/jdbc_utilexample.java
@@ -0,0 +1,55 @@
+package jdbctests;
+
+import org.testng.annotations.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class jdbc_utilexample {
+
+    String dbUrl = "jdbc:oracle:thin:@54.161.128.36:1521:xe";
+    String dbUsername = "hr";
+    String dbPassword = "hr";
+    @Test
+    public void metadata() throws SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("select * from departments");
+
+        ResultSetMetaData rsMetadata = resultSet.getMetaData();
+
+        System.out.println("Column count:"+rsMetadata.getColumnCount());
+
+        int columnCount = rsMetadata.getColumnCount();
+        for (int i = 1; i <=columnCount; i++) {
+            //System.out.println(rsMetadata.getColumnName(i));
+        }
+        List<Map<String,Object>> queryData = new ArrayList<>();
+        Map<String,Object> row1 = new HashMap<>();
+        row1.put("first_name","Steven");
+        row1.put("last_name","King");
+        row1.put("salary",24000);
+        row1.put("job_id","AD_PRESS");
+        System.out.println(row1.toString());
+        // get the first
+        System.out.println(row1.get("first_name"));
+        //--------------get row 2
+        Map<String,Object> row2 = new HashMap<>();
+        row2.put("first_name","Neena");
+        row2.put("last_name","Kochhar");
+        row2.put("salary",17000);
+        row2.put("job_id","AD_VP");
+        //adding rows to my list
+        queryData.add(row1);
+        queryData.add(row2);
+        System.out.println("Neena Salary: "+queryData.get(1).get("salary"));
+        System.out.println("Steven JobId: "+queryData.get(0).get("job_id"));
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+}
